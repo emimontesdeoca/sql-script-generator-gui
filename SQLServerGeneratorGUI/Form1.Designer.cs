8c77f5cf5dd8634598711efde94492e8f148c627
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2019-08-30 12:51:26 +0100

    Initial files

---
+++ SQLServerGeneratorGUI/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SQLServerGeneratorGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.input_server = new System.Windows.Forms.TextBox();
+            this.input_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input_password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView_sql = new System.Windows.Forms.DataGridView();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.comboBox_databases = new System.Windows.Forms.ComboBox();
+            this.button_savetofile = new System.Windows.Forms.Button();
+            this.button_savetoclipboard = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_sql)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // input_server
+            // 
+            this.input_server.Location = new System.Drawing.Point(82, 6);
+            this.input_server.Name = "input_server";
+            this.input_server.Size = new System.Drawing.Size(146, 20);
+            this.input_server.TabIndex = 1;
+            // 
+            // input_username
+            // 
+            this.input_username.Location = new System.Drawing.Point(82, 32);
+            this.input_username.Name = "input_username";
+            this.input_username.Size = new System.Drawing.Size(146, 20);
+            this.input_username.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username";
+            // 
+            // input_password
+            // 
+            this.input_password.Location = new System.Drawing.Point(82, 58);
+            this.input_password.Name = "input_password";
+            this.input_password.Size = new System.Drawing.Size(146, 20);
+            this.input_password.TabIndex = 5;
+            this.input_password.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password";
+            // 
+            // dataGridView_sql
+            // 
+            this.dataGridView_sql.AllowUserToAddRows = false;
+            this.dataGridView_sql.AllowUserToDeleteRows = false;
+            this.dataGridView_sql.AllowUserToResizeRows = false;
+            this.dataGridView_sql.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_sql.Location = new System.Drawing.Point(12, 85);
+            this.dataGridView_sql.Name = "dataGridView_sql";
+            this.dataGridView_sql.ShowEditingIcon = false;
+            this.dataGridView_sql.Size = new System.Drawing.Size(798, 425);
+            this.dataGridView_sql.TabIndex = 6;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(688, 11);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(122, 29);
+            this.button_connect.TabIndex = 7;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.Button_connect_Click);
+            // 
+            // comboBox_databases
+            // 
+            this.comboBox_databases.FormattingEnabled = true;
+            this.comboBox_databases.Location = new System.Drawing.Point(664, 57);
+            this.comboBox_databases.Name = "comboBox_databases";
+            this.comboBox_databases.Size = new System.Drawing.Size(146, 21);
+            this.comboBox_databases.TabIndex = 8;
+            this.comboBox_databases.SelectedIndexChanged += new System.EventHandler(this.ComboBox_databases_SelectedIndexChanged);
+            // 
+            // button_savetofile
+            // 
+            this.button_savetofile.Location = new System.Drawing.Point(706, 515);
+            this.button_savetofile.Name = "button_savetofile";
+            this.button_savetofile.Size = new System.Drawing.Size(104, 35);
+            this.button_savetofile.TabIndex = 9;
+            this.button_savetofile.Text = "Save to file";
+            this.button_savetofile.UseVisualStyleBackColor = true;
+            this.button_savetofile.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button_savetoclipboard
+            // 
+            this.button_savetoclipboard.Location = new System.Drawing.Point(596, 515);
+            this.button_savetoclipboard.Name = "button_savetoclipboard";
+            this.button_savetoclipboard.Size = new System.Drawing.Size(104, 35);
+            this.button_savetoclipboard.TabIndex = 10;
+            this.button_savetoclipboard.Text = "Save to clipboard";
+            this.button_savetoclipboard.UseVisualStyleBackColor = true;
+            this.button_savetoclipboard.Click += new System.EventHandler(this.Button_savetoclipboard_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(594, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Database";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 562);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_savetoclipboard);
+            this.Controls.Add(this.button_savetofile);
+            this.Controls.Add(this.comboBox_databases);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.dataGridView_sql);
+            this.Controls.Add(this.input_password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.input_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.input_server);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SqlScriptGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_sql)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input_server;
+        private System.Windows.Forms.TextBox input_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox input_password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView_sql;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.ComboBox comboBox_databases;
+        private System.Windows.Forms.Button button_savetofile;
+        private System.Windows.Forms.Button button_savetoclipboard;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SQLServerGeneratorGUI/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLServerGeneratorGUI
+{
+    public partial class Form1 : Form
+    {
+        public static string USERNAME;
+        public static string PASSWORD;
+        public static string SERVER;
+        public static string SELECTED_DATABASE;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox_databases.Enabled = false;
+            dataGridView_sql.Enabled = false;
+            button_savetoclipboard.Enabled = false;
+            button_savetofile.Enabled = false;
+
+            dataGridView_sql.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void Button_connect_Click(object sender, EventArgs e)
+        {
+            comboBox_databases.Enabled = false;
+            dataGridView_sql.Enabled = false;
+            button_savetoclipboard.Enabled = false;
+            button_savetofile.Enabled = false;
+
+            SERVER = input_server.Text;
+            USERNAME = input_username.Text;
+            PASSWORD = input_password.Text;
+
+            var databases = Utils.GetDatabaseList(SERVER, USERNAME, PASSWORD);
+            dataGridView_sql.Rows.Clear();
+
+            if (databases == null)
+            {
+                MessageBox.Show("Couldn't get databases", "SqlScriptGenerator");
+            }
+            else
+            {
+                MessageBox.Show("Databases loaded successfully", "SqlScriptGenerator");
+
+                dataGridView_sql.Rows.Clear();
+                comboBox_databases.Items.Clear();
+                comboBox_databases.Items.Add("Select database");
+                comboBox_databases.Items.AddRange(databases.ToArray());
+                comboBox_databases.SelectedIndex = 0;
+
+                comboBox_databases.Enabled = true;
+            }
+        }
+
+        private void ComboBox_databases_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox_databases.SelectedItem.ToString() == "Select database")
+            {
+                return;
+            }
+
+            try
+            {
+                SELECTED_DATABASE = comboBox_databases.SelectedItem.ToString();
+                var spfuncs = Utils.GetSPandFunctions(SERVER, USERNAME, PASSWORD, SELECTED_DATABASE);
+
+                dataGridView_sql.DataSource = spfuncs;
+                dataGridView_sql.Enabled = true;
+                button_savetoclipboard.Enabled = true;
+                button_savetofile.Enabled = true;
+
+                dataGridView_sql.Columns[0].Width = 50;
+                dataGridView_sql.Columns[0].ReadOnly = true;
+
+                dataGridView_sql.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView_sql.Columns[1].ReadOnly = true;
+
+                dataGridView_sql.Columns[2].Width = 100;
+                dataGridView_sql.Columns[2].ReadOnly = true;
+
+                dataGridView_sql.Columns[3].Width = 50;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error: " + ex.Message, "SqlScriptGenerator");
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var list = GetData();
+            var finalList = new List<string>();
+            list.ForEach(x => finalList.AddRange(x));
+
+            if (list.Count == 0)
+            {
+                MessageBox.Show("No items selected", "SqlScriptGenerator");
+            }
+            else
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    fbd.Description = "Select where you want to save the generated SQL script";
+
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        try
+                        {
+                            var filename = $"{SELECTED_DATABASE}-queries-{DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss")}.sql";
+                            Utils.SaveToFile(Path.Combine(fbd.SelectedPath, filename), finalList);
+                            MessageBox.Show("Saved successfully", "SqlScriptGenerator");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("There was an error: " + ex.Message, "SqlScriptGenerator");
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+
+        private void Button_savetoclipboard_Click(object sender, EventArgs e)
+        {
+            var list = GetData();
+            var finalString = "";
+
+            foreach (var item in list)
+            {
+                finalString += string.Join("", item.ToArray());
+            }
+
+            if (!String.IsNullOrEmpty(finalString))
+            {
+                Clipboard.SetText(finalString);
+                MessageBox.Show("Copied to clipboard successfully", "SqlScriptGenerator");
+            }
+            else
+            {
+                MessageBox.Show("No items selected", "SqlScriptGenerator");
+            }
+        }
+
+        private List<List<string>> GetData()
+        {
+            var connectionstring = $"Data Source={SERVER};Initial Catalog={SELECTED_DATABASE};Persist Security Info=True;User ID={USERNAME};Password={PASSWORD};Connect Timeout=60";
+            var itemCount = dataGridView_sql.Rows.Count;
+            var dataToGenerate = new Dictionary<string, List<string>>();
+            var finalList = new List<List<string>>();
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                var needGeneration = Boolean.Parse(dataGridView_sql.Rows[i].Cells[3].Value.ToString());
+
+                if (needGeneration)
+                {
+                    var item = $"{dataGridView_sql.Rows[i].Cells[0].Value.ToString()}.{dataGridView_sql.Rows[i].Cells[1].Value.ToString()}";
+                    dataToGenerate.Add(item, Utils.GetResultOfFunction(item, connectionstring));
+                }
+            }
+
+            foreach (var item in dataToGenerate)
+            {
+                finalList.Add(Utils.GetListToSave(item.Value, item.Key, SELECTED_DATABASE, USERNAME));
+            }
+
+            return finalList;
+        }
+    }
+}
